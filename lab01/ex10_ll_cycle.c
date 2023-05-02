daba1dbce1370cb9ce4fbbaa18d6d82d792e9f32
mkx000 <3080233675@qq.com>
2023-05-02 16:43:36 +0800

    lab01

--- lab01/ex10_ll_cycle.c
+++ lab01/ex10_ll_cycle.c
@@ -2,5 +2,23 @@
 #include "ex10_ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* TODO: Implement ll_has_cycle */
+    node* fast = head;
+    node* slow = head;
+    while(fast) {
+        fast = fast->next;
+        if (!fast) {
+            return 0;
+        }
+        fast = fast->next;
+        slow = slow->next;
+
+        if (fast == slow) {
+            return 1;
+        }
+    }
+
+    return 0;
 }
+
+
+
